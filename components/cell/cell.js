56eb0f6f220c1461ea39345a888f40287072ba7b
rjhee <qkdrkwogml@gmail.com>
2022-12-26 23:57:20 +0900

    update style onfocus cell

--- components/cell/cell.js
+++ components/cell/cell.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from './cell.module.css';
 
 const Cell = (props) => {
     const {r, c} = props;
+    const [focus, setFocus] = useState(false);
+    const focusStyle = {boxSizing:"border-box", border: '2px #20dad8 solid'};
+
     return (
-        <div className={styles.cell}>
+        <div className={styles.cell} style={focus ? focusStyle : {}}>
             <input
                 type="text"
-                   onClick={()=>{
-                console.log('cell.js:셀 위치',c?.toUpperCase() + r);
-
+                onFocus={()=>setFocus(true)}
+                onFocusCapture={()=> setFocus(false)}
+                onClick={()=>{
+                    console.log('cell.js:셀 위치',c?.toUpperCase() + r);
             }}/>
         </div>
     );
